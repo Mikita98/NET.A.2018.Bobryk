4da2f9ca298fcf3b39cbb157c5a937be1940f54e
Mikita <memesnotwar@gmail.com>
2018-10-18 23:27:09 +0300

    Day 4 task done
    Реализовать метод TransformToWords который принимает массив вещественных чисел и трансформирует его в массив строк таким образом, чтобы каждое вещественное число преобразовывалось в его "словестный формат". Например, FilterDigit (-23.809, 0.295, 15.17) -> {"minus two three point eight zero nine", "zero point two nine five", "one five point one seven"}.

---
+++ NET.A.2018.Bobryk.4/NET.A.2018.Bobryk.4/Transforming.NUTest/TransformingToWordsNUTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Transforming.NUTest
+{
+    [TestFixture]
+    public class TransformingToWordsNUTests
+    {
+        [Test]
+        [TestCase(-2.4, ExpectedResult = "minus two point four")]
+        [TestCase(0, ExpectedResult = "zero")]
+        [TestCase(-10956.4567, ExpectedResult = "minus one zero nine five six point four five six seven")]
+        [TestCase(100, ExpectedResult = "one zero zero")]
+        [TestCase(.4, ExpectedResult = "zero point four")]
+        [TestCase(+8, ExpectedResult = "eight")]
+        [TestCase(143567.9087654, ExpectedResult = "one four three five six seven point nine zero eight seven six five four")]
+        public string TranformingToWordsTest(double number)
+        {
+            string result = TransformingToWords.TransformToWords(number);
+                return result;
+        }
+    }
+}
---
+++ NET.A.2018.Bobryk.4/NET.A.2018.Bobryk.4/Transforming/TransformingToWords.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transforming
+{
+    /// <summary>
+    /// TransformingToWords Class that have method 
+    /// for transform double number to string on human language
+    /// </summary>
+    public static class TransformingToWords
+    {
+        /// <summary>
+        /// Transform double number to string on human language
+        /// </summary>
+        /// <param name ="number">
+        ///  double number for transform
+        /// </param>
+        /// <returns>string on human language</returns>
+        public static string TransformToWords(double number)
+        {
+            string[] dictionary = new string[] 
+            {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+            };
+            char[] char_number = DoubleToCharArray(number);
+
+            string result = string.Empty;
+
+            for (int i = 0; i < char_number.Length; i++)
+            {
+                if (char_number[i].ToString() == ".")
+                {
+                    AddSymbol(ref result, "point");
+                    continue;
+                }
+
+                if (char_number[i].ToString() == "-")
+                {
+                    AddSymbol(ref result, "minus");
+                    continue;
+                }
+                else
+                {
+                    AddSymbol(ref result, dictionary[Convert.ToInt32(char_number[i]) - 48]);
+                }
+            }
+
+            result = result.TrimEnd();
+            return result;
+        }
+
+        private static void AddSymbol(ref string result, string symbol)
+        {
+            result += symbol + " ";
+        }
+
+        private static char[] DoubleToCharArray(double number)
+        {
+            string string_number = number.ToString("G");
+            char[] char_number = string_number.ToCharArray();
+                return char_number;
+        }
+    }
+}
