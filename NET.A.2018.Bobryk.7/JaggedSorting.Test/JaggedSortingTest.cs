60982c1d0719f807b9819408e0d78ff2c8b81bbc
Mikita <memesnotwar@gmail.com>
2018-10-25 00:29:28 +0300

    Day 7 task done

---
+++ NET.A.2018.Bobryk.7/JaggedSorting.Test/JaggedSortingTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using JaggedSorting;
+
+namespace JaggedSorting.Test
+{
+    [TestFixture]
+    public class JaggedSortingTest
+    {
+        [TestCase(new int[] { 1, 3, 5, 7, 7 }, new int[] { 4, 8, 6, 10, 3 }, new int[] { -1, 4, 2, 9, 0 })]
+        [TestCase(new int[] { 17, 35, 33, 7, 9 }, new int[] { 4, 8, 6, 100, 3 }, new int[] { -1, 0, 53, -7, })]
+        [TestCase(null, new int[] { 4, 8, 6, 10, 3 }, null)]
+        public void MaxSortingTest_ValidData_ValidResult(int[] array0, int[] array1, int[] array2)
+        {
+            int[][] array = new int[3][];
+            array[0] = array0;
+            array[1] = array1;
+            array[2] = array2;
+            int[][] expected = new int[3][];
+            expected[0] = array1;
+            expected[1] = array2;
+            expected[2] = array0;
+            JaggedSorting.MaxValueSorting(ref array);
+            CollectionAssert.AreEqual(array, expected);
+        }
+
+        [TestCase(new int[] { -100, 4, 2, 9, }, new int[] { -6, 4, 2, 9, }, new int[] { 4, 8, -2, 10, 3 }, new int[] { -1, 3, 5, 7, 7 })]
+        [TestCase(new int[] { 9, 4, 2, -100 }, new int[] { -50, 4, 2, 9, }, new int[] { 4, 8, -23, 10, 3 }, new int[] { 0, 3, 5, 7, 7 })]
+        [TestCase(null, new int[] { -6, 4, 2, 9, }, new int[] { 4, 8, -2, 10, 3 }, new int[] { -1, 3, 5, 7, 7 })]
+        public void MinSortingTest_ValidData_ValidResult(int[] array0, int[] array1, int[] array2, int[] array3)
+        {
+            int[][] array = new int[4][];
+            array[0] = array0;
+            array[1] = array1;
+            array[2] = array2;
+            array[3] = array3;
+            int[][] expected = new int[4][];
+            expected[0] = array3;
+            expected[1] = array2;
+            expected[2] = array1;
+            expected[3] = array0;
+            JaggedSorting.MinValueSorting(ref array);
+            CollectionAssert.AreEqual(array, expected);
+        }
+
+        [TestCase(new int[] { 0, 4, 2, 9, }, new int[] { 234, 4, 46, 9, }, new int[] { 4, 8, 2, 460, 3 }, new int[] { 1, 1000, 5, 7, 7 })]
+        [TestCase(new int[] { 0, -4, 2, -9, }, new int[] { 0, 4, 46, 9, }, new int[] { 4, 8, 2, 460, 3 }, new int[] { 1, 1000, 5, 7, 7 })]
+        [TestCase(null, null, new int[] { 4, 8, 2, 460, 3 }, new int[] { 1, 1000, 5, 7, 7 })]
+        public void SumSortingTest_ValidData_ValidResult(int[] array0, int[] array1, int[] array2, int[] array3)
+        {
+            int[][] array = new int[4][];
+            array[0] = array0;
+            array[1] = array1;
+            array[2] = array2;
+            array[3] = array3;
+            int[][] expected = new int[4][];
+            expected[0] = array3;
+            expected[1] = array2;
+            expected[2] = array1;
+            expected[3] = array0;
+            JaggedSorting.SumValueSorting(ref array);
+            CollectionAssert.AreEqual(array, expected);
+        }
+    }
+}
---
+++ NET.A.2018.Bobryk.7/JaggedSorting/JaggedSorting.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedSorting
+{
+    public static class JaggedSorting
+    {
+        /// <summary>
+        /// Sort method Array by Max value
+        /// </summary>
+        /// <param name="array"</param>
+        /// <exception cref="ArgumentNullException">
+        /// If array should not be represent as null array
+        /// </exception>
+        public static void MaxValueSorting(ref int[][] array)
+        {
+           CheckData(array);
+           for (int i  = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    CompareMax(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sort method Array by Mun value
+        /// </summary>
+        /// <param name="array"</param>
+        /// <exception cref="ArgumentNullException">
+        /// If array should not be represent as null array
+        /// </exception>
+        public static void MinValueSorting(ref int[][] array)
+        {
+            CheckData(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    CompareMin(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sort method Array by Sum value
+        /// </summary>
+        /// <param name="array"</param>
+        /// <exception cref="ArgumentNullException">
+        /// If array should not be represent as null array
+        /// </exception>
+        public static void SumValueSorting(ref int[][] array)
+        {
+            CheckData(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    CompareSum(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+
+        private static void CheckData(int[][] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array), "Null is invalid type for this array.");
+            }
+        }
+
+        private static int Compare(int first, int second)
+        {
+            if (first >= second)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private static void SwapArrays(ref int[] first, ref int[] second, int? sign)
+        {
+            if (sign == 0)
+            {
+                Swap(ref first, ref second);
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private static void CompareMin(ref int[] first, ref int[] second)
+        {
+            int? temp = CheckForNuLL(first, second);
+            if (temp != null)
+            {
+                SwapArrays(ref first, ref second, temp);
+            }
+            else
+            {
+                temp = Compare(first.Min(), second.Min());
+                SwapArrays(ref first, ref second, (int)temp);
+            }
+        }
+
+        private static void CompareMax(ref int[] first, ref int[] second)
+        {
+            int? temp = CheckForNuLL(first, second);
+            if (temp != null)
+            {
+                SwapArrays(ref first, ref second, temp);
+            }
+            else
+            {
+                temp = Compare(first.Max(), second.Max());
+                SwapArrays(ref first, ref second, (int)temp);
+            }
+        }
+
+        private static void CompareSum(ref int[] first, ref int[] second)
+        {
+            int? temp = CheckForNuLL(first, second);
+            if (temp != null)
+            {
+                SwapArrays(ref first, ref second, temp);
+            }
+            else
+            {
+                temp = Compare(first.Sum(), second.Sum());
+                SwapArrays(ref first, ref second, (int)temp);
+            }
+        }
+
+        private static int? CheckForNuLL(int[] first, int[] second)
+        {
+            if ((second == null && first == null) || (second == null))
+            {
+                return 1;
+            }
+            else if (first == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static void Swap(ref int[] first, ref int[] second)
+        {
+            int[] temp = first;
+            first = second;
+            second = temp;
+        }
+    }
+}
