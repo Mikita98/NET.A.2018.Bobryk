d66edb40b77123d8fa8cc6b17ef1b4131529a06a
Mikita <memesnotwar@gmail.com>
2018-10-31 23:48:45 +0300

    add clases in task Day 8

---
+++ NET.A.2018.Bobryk.8/BankAccount/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public sealed class BankSingleton
+    {
+        private static readonly Lazy<BankSingleton> _instance = new Lazy<BankSingleton>(() => new BankSingleton());
+
+        public List<int> accountsId;
+        public List<int> holdersId;
+
+        BankSingleton() { }
+
+        public static BankSingleton Instance { get { return _instance.Value; } }
+    }
+
+
+    public abstract class BankAccount
+    {
+        public readonly int accountId;
+        public List<int> holdersId;
+
+        public abstract void DeleteHolder();
+
+        public abstract void AddHolder();
+
+        private int CreateAccountId()
+        {
+            BankSingleton singleton = BankSingleton.Instance;
+            Random random = new Random();
+            int id = random.Next(0, int.MaxValue);
+
+            while (singleton.holdersId.Contains(id))
+            {
+                id = random.Next(0, int.MaxValue);
+            }
+
+            return id;
+        }
+        public BankAccount(Holder.Holder holder)
+        {
+            holdersId = new List<int>();
+            holdersId.Add(holder.holderId);
+            accountId = CreateAccountId();
+        }
+    }
+
+    public class BaseAcount : BankAccount
+    {
+        public override void AddHolder()
+        {
+            throw new NotImplementedException(); 
+        }
+
+        public override void DeleteHolder()
+        {
+            throw new NotImplementedException();
+        }
+
+        BaseAcount(Holder.Holder holder) : base(holder)
+        {
+        }
+
+    }
+}
---
+++ NET.A.2018.Bobryk.8/Holder/Holder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holder
+{
+    public class Holder
+    {
+        public string firstName { get; private set; }
+        public string SurName { get; private set; }
+        public string patronymic { get; private set; }
+        public string email { get; private set; }
+
+        public readonly int holderId;
+
+        Holder(string firstName, string SurName, string patronymic, string email)
+        {
+            this.firstName = firstName;
+            this.SurName = SurName;
+            this.patronymic = patronymic;
+            this.email = email;
+        }
+    }
+}
